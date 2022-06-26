38339979f878be42b1b068669b80437115c26025
Shrima Narasimhan <shrima.narasimhan@gmail.com>
2022-06-25 17:35:13 -0700

    Created index.html and script.js files. Updated both. HTML code is done but more needs to be added to script.js.

---
+++ script.js
@@ -0,0 +1,11 @@
+//button post-click action
+
+var myButton = $('button');
+myButton.on("click", submitAction);
+
+function submitAction() {
+
+  //logic for sending Brand User Input Picture to ML Model
+
+}
+
